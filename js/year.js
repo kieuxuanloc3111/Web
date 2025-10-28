e014d984751bb552d17b99142f543403d2cdaa80
kieuxuanloc3111 <chokeslam311@gmail.com>
2025-10-28 22:12:07 +0700

    a

---
+++ js/year.js
@@ -0,0 +1,38 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function kiemTraNam(nam) {
+    const so = parseInt(nam);
+
+    if (isNaN(so)) {
+        console.log("Vui lòng nhập một số hợp lệ!");
+        return;
+    }
+
+    if (so % 2 === 0) {
+        console.log(`Đây là năm chẵn`);
+    } else {
+        console.log(`Đây là năm lẻ`);
+    }
+}
+
+function nhapNam() {
+    rl.question("Nhập năm: ", (nam) => {
+        if (nam.trim() === "") {
+            console.log("Bạn chưa nhập năm nào!");
+        } else if (nam.toLowerCase().trim() === "thoát") {
+            console.log("Tạm biệt!");
+            rl.close();
+            return;
+        } else {
+            kiemTraNam(nam);
+        }
+        nhapNam(); 
+    });
+}
+
+nhapNam();
