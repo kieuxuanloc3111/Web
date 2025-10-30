02f07b0f08ea6343797e0cc9da96a7f69ef7a51a
kieuxuanloc3111 <kieuxuanloc3111@gmail.com>
2025-10-30 09:05:17 +0700

    a

--- js/thu.js
+++ js/thu.js
@@ -1,41 +1,48 @@
-for(let i=1;i<=10;i++){
-
-    console.log(i);
-}
-
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 !== 0) {
+function inTu1DenN(n) {
+    for (let i = 1; i <= n; i++) {
         console.log(i);
     }
 }
 
-for (let i = 1; i <= 10; i++) {
-    if (i<5){
-        console.log(i)
+function inSoLe(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 !== 0) {
+            console.log(i);
+        }
     }
 }
 
-for (let i=1;i<=10;i++){
-    if (i===6){
-        console.log(i)
+function inSoNhoHon5(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i < 5) {
+            console.log(i);
+        }
     }
 }
 
-let tong=0;
-for(let i=1; i<=10;i++){
-    tong +=i
-}
-console.log(tong)
-
-
-let n = 5;
-let s = 0;
-
-for (let i = 1; i <= n; i++) {
-    s += 1 / i;
+function inSoBang6(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i === 6) {
+            console.log(i);
+        }
+    }
 }
 
-console.log(s);
+function tinhTong(n) {
+    let tong = 0;
+    for (let i = 1; i <= n; i++) {
+        tong += i;
+    }
+    console.log(`Tổng từ 1 đến ${n} là: ${tong}`);
+}
+
+function tinhTongPhanSo(n) {
+    let s = 0;
+    for (let i = 1; i <= n; i++) {
+        s += 1 / i;
+    }
+    console.log(`Tổng S = ${s}`);
+}
 
 function timSoLonNhat(a, b, c) {
     let max;
@@ -52,3 +59,6 @@ function timSoLonNhat(a, b, c) {
 }
 
 timSoLonNhat(1,2,3);
+
+
+giờ bạn tạo cho mình cái html , với mỗi hàm bạn cho 1 cái h1 mô tả : ví dụ : tính tổng từ 1 tới n , rồi làm 1 cái nút và 1 cái khung để nhập n, khi nhập n rồi bấm sẽ đưa ra kết 
