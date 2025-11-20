86600249c8582d3c329116784990115892e4d2f8
kieuxuanloc3111 <kieuxuanloc3111@gmail.com>
2025-11-20 09:31:10 +0700

    a

--- hoc react js/phplearn/src/Component/Blog_detail.js
+++ hoc react js/phplearn/src/Component/Blog_detail.js
@@ -1,105 +1,139 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
+import React from 'react'
 
 const Blog_detail = () => {
-  const params = useParams(); // lấy id trên URL
-  const [data, setData] = useState(null);
-  const [comment, setComment] = useState([]);
-
-  useEffect(() => {
-    const fetchDetail = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost/laravel8/laravel8/public/api/blog/detail/" + params.id
-        );
-
-        setData(res.data.data);
-        setComment(res.data.data.comment || []);
-      } catch (error) {
-        console.log("Lỗi API:", error);
-      }
-    };
-
-    fetchDetail();
-  }, [params.id]);
-
-  if (!data) {
-    return <p>Đang tải dữ liệu...</p>;
-  }
-
   return (
-    <section>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-9">
-            <div className="blog-post-area">
-              <h2 className="title text-center">{data.title}</h2>
-
-              <div className="single-blog-post">
-
-                {/* TITLE */}
-                <h3>{data.title}</h3>
-
-                {/* META */}
-                <div className="post-meta">
-                  <ul>
-                    <li><i className="fa fa-user" /> Tác giả ID: {data.id_auth}</li>
-                    <li><i className="fa fa-calendar" /> {data.created_at}</li>
-                  </ul>
-                </div>
-
-                {/* IMAGE */}
-                <img
-                  src={`http://localhost/laravel8/laravel8/public/upload/Blog/image/${data.image}`}
-                  alt=""
-                  style={{ width: "100%", marginBottom: "20px" }}
-                />
-
-                {/* DESCRIPTION */}
-                <p>{data.description}</p>
-
-                {/* CONTENT (đã là HTML nên phải render bằng dangerouslySetInnerHTML) */}
-                <div
-                  dangerouslySetInnerHTML={{ __html: data.content }}
-                ></div>
-
-              </div>
-            </div>
-
-            {/* COMMENTS */}
-            <div className="response-area">
-              <h2>{comment.length} RESPONSES</h2>
-
-              <ul className="media-list">
-                {comment.map((cmt, index) => (
-                  <li className="media" key={index}>
-                    <a className="pull-left" href="#">
-                      <img
-                        className="media-object"
-                        src={`/frontend/images/blog/man-two.jpg`}
-                        alt=""
-                      />
-                    </a>
-
-                    <div className="media-body">
-                      <ul className="sinlge-post-meta">
-                        <li><i className="fa fa-user" /> {cmt.name}</li>
-                        <li><i className="fa fa-calendar" /> {cmt.created_at}</li>
-                      </ul>
-
-                      <p>{cmt.comment}</p>
-                    </div>
-                  </li>
-                ))}
+    <div>
+<section>
+  <div className="container">
+    <div className="row">
+      <div className="col-sm-9">
+        <div className="blog-post-area">
+          <h2 className="title text-center">Latest From our Blog</h2>
+          <div className="single-blog-post">
+            <h3>Girls Pink T Shirt arrived in store</h3>
+            <div className="post-meta">
+              <ul>
+                <li><i className="fa fa-user" /> Mac Doe</li>
+                <li><i className="fa fa-clock-o" /> 1:33 pm</li>
+                <li><i className="fa fa-calendar" /> DEC 5, 2013</li>
               </ul>
             </div>
-
+            <a href="">
+              <img src="/frontend/images/blog/blog-one.jpg" alt="" />
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p> <br />
+            <p>
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p> <br />
+            <p>
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p> <br />
+            <p>
+              Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+            </p>
+            <div className="pager-area">
+              <ul className="pager pull-right">
+                <li><a href="#">Pre</a></li>
+                <li><a href="#">Next</a></li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
-};
 
-export default Blog_detail;
+        <div className="rating-area">
+          <ul className="ratings">
+            <li className="rate-this">Rate this item:</li>
+            <li>
+              <i className="fa fa-star color" />
+              <i className="fa fa-star color" />
+              <i className="fa fa-star color" />
+              <i className="fa fa-star" />
+              <i className="fa fa-star" />
+            </li>
+            <li className="color">(6 votes)</li>
+          </ul>
+          <ul className="tag">
+            <li>TAG:</li>
+            <li><a className="color" href="">Pink <span>/</span></a></li>
+            <li><a className="color" href="">T-Shirt <span>/</span></a></li>
+            <li><a className="color" href="">Girls</a></li>
+          </ul>
+        </div>
+
+        <div className="socials-share">
+          <a href=""><img src="/frontend/images/blog/socials.png" alt="" /></a>
+        </div>
+
+        <div className="response-area">
+          <h2>3 RESPONSES</h2>
+          <ul className="media-list">
+            <li className="media">
+              <a className="pull-left" href="#">
+                <img className="media-object" src="/frontend/images/blog/man-two.jpg" alt="" />
+              </a>
+              <div className="media-body">
+                <ul className="sinlge-post-meta">
+                  <li><i className="fa fa-user" />Janis Gallagher</li>
+                  <li><i className="fa fa-clock-o" /> 1:33 pm</li>
+                  <li><i className="fa fa-calendar" /> DEC 5, 2013</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a className="btn btn-primary" href=""><i className="fa fa-reply" />Replay</a>
+              </div>
+            </li>
+            <li className="media second-media">
+              <a className="pull-left" href="#">
+                <img className="media-object" src="/frontend/images/blog/man-three.jpg" alt="" />
+              </a>
+              <div className="media-body">
+                <ul className="sinlge-post-meta">
+                  <li><i className="fa fa-user" />Janis Gallagher</li>
+                  <li><i className="fa fa-clock-o" /> 1:33 pm</li>
+                  <li><i className="fa fa-calendar" /> DEC 5, 2013</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a className="btn btn-primary" href=""><i className="fa fa-reply" />Replay</a>
+              </div>
+            </li>
+            <li className="media">
+              <a className="pull-left" href="#">
+                <img className="media-object" src="/frontend/images/blog/man-four.jpg" alt="" />
+              </a>
+              <div className="media-body">
+                <ul className="sinlge-post-meta">
+                  <li><i className="fa fa-user" />Janis Gallagher</li>
+                  <li><i className="fa fa-clock-o" /> 1:33 pm</li>
+                  <li><i className="fa fa-calendar" /> DEC 5, 2013</li>
+                </ul>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <a className="btn btn-primary" href=""><i className="fa fa-reply" />Replay</a>
+              </div>
+            </li>
+            
+          </ul>					
+        </div>
+
+        <div className="replay-box">
+          <div className="row">
+            <div className="col-sm-12">
+              <h2>Leave a replay</h2>
+              <div className="text-area">
+                <div className="blank-arrow">
+                  <label>Your Name</label>
+                </div>
+                <span>*</span>
+                <textarea name="message" rows={11} defaultValue={""} />
+                <a className="btn btn-primary" href="">post comment</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>	
+    </div>
+  </div>
+</section>
+
+    </div>
+  )
+}
+
+export default Blog_detail
