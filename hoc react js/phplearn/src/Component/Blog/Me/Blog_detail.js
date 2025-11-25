b087fc974f5962e8e8e322d18ebef54cb3555799
kieuxuanloc3111 <chokeslam311@gmail.com>
2025-11-25 10:31:22 +0700

    a

--- hoc react js/phplearn/src/Component/Blog/Me/Blog_detail.js
+++ hoc react js/phplearn/src/Component/Blog/Me/Blog_detail.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import Rate from "../Rate";
 import ListComment from "./List_comment";
 import Comment from "./Comment";
-import List_comment2 from "../List_comment2";
+
 
 const Blog_detail = () => {
   const params = useParams();
